ed055af7337717f2ce1bd5b5bbd7e81116c219e6
Saniyakanwal <160118062+Saniyakanwal@users.noreply.github.com>
2024-11-07 18:28:13 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ components/header.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+
+const Header = () => {
+  return (
+    <div className='bg-[#A29875] h-[134px]'>
+      <h1 className='text-[75px]  font-rye pt-5 pl-9 h-[94px] w-[439px] text-white'>MANZZARI</h1>
+      
+    </div>
+  )
+}
+
+export default Header
